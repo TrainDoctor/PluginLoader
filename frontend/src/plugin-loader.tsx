86597ac7b59896c93104b9ab4d233753c996ff10
botato <botatooo@protonmail.com>
2022-09-01 22:07:43 -0400

    stuff

--- frontend/src/plugin-loader.tsx
+++ frontend/src/plugin-loader.tsx
@@ -252,6 +252,8 @@ class PluginLoader extends Logger {
   async _uninstallDecky(keepPlugins: boolean) {
     console.log('uninstall call from frontend');
     this.unloadAll();
+    // this.tabsHook.deinit();
+    // this.routerHook.deinit();
     const res = await this.callServerMethod('uninstall_decky', { keepPlugins });
     console.log('uninstall done from frontend');
     this.toaster.toast({
@@ -259,6 +261,8 @@ class PluginLoader extends Logger {
       body: res.success ? 'Uninstalled successfully!' : 'Uninstallation failed',
       critical: !res.success,
     });
+    // this.toaster.deinit();
+    // this.deinit();
   }
 
   async callServerMethod(methodName: string, args = {}): Promise<{ res: string; success: boolean }> {
